8e568d0e741e264b61ce71d5dbd22937eb61ac18
Dixon Calderon <dixon.calderon0@gmail.com>
2020-04-10 02:03:08 -0500

    Intercambio inicial de proyecto

---
+++ Sensor.java
@@ -0,0 +1,28 @@
+public class Sensor{
+    
+    private static Sensor[] sensores;
+    private static int tamano;
+    private static int posAnadir;
+    private String tipo;
+    private double valor;
+    
+    public Sensor(){
+        
+    }
+    
+    public Sensor(String t, double v){
+        
+    }
+    
+    public String toString(){
+    
+    }
+    
+    public static String toStringSensores(){
+    
+    }
+    
+    public static int cantidadSensores(){
+    
+    }
+}
---
+++ Vehiculo.java
@@ -0,0 +1,34 @@
+public class Vehiculo{
+    
+    public static Vehiculo[] vehiculos;
+    public static int tamano;
+    public static int posAnadir;
+    private int modelo;
+    private String marca;
+    private double valorComercial;
+    private String color;
+    
+    public Vehiculo(){
+        
+    }
+    
+    public Vehiculo(int mo, String ma, double va){
+        
+    }
+    
+    public Vehiculo(int mo, String ma, double va, String co){
+        
+    }
+    
+    public String toString(){
+    
+    }
+    
+    public static String toStringVehiculos(){
+    
+    }
+    
+    public static int cantidadVehiculos(){
+    
+    }
+}
